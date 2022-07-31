9358a7aacf7acaa7af12abdebe7788c8c0729546
bonannogiovanni <bonannogiovanni@hotmail.com>
2022-07-31 18:50:41 +0200

    Initial commit

---
+++ test/staging/FundMe.staging.test.js
@@ -0,0 +1,27 @@
+const { network, ethers, getNamedAccounts } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
+const { assert, expect } = require("chai")
+
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("FundMe", async function () {
+          let fundMe
+          let deployer
+
+          const sendValue = ethers.utils.parseEther("0.1")
+
+          beforeEach(async function () {
+              deployer = (await getNamedAccounts()).deployer
+              // await deployments.fixture(["all"])
+              fundMe = await ethers.getContract("FundMe", deployer)
+          })
+
+          it("allows people to fund and withdraw", async function () {
+              await fundMe.fund({ value: sendValue })
+              await fundMe.withdraw()
+              const endingFundMeBalance = await fundMe.provider.getBalance(
+                  fundMe.address
+              )
+              assert.equal(endingFundMeBalance.toString(), "0")
+          })
+      })
